46c0aba6e34291d286c7fb22508ad4f9a068b8fd
eKlusha <noobrus86@yandex.ru>
2023-08-21 00:24:42 +0500

    hw6 end

--- src/main/java/ru/otus/java/basic/homeworks/homework6/Cat.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework6/Cat.java
@@ -3,18 +3,20 @@ package ru.otus.java.basic.homeworks.homework6;
 class Cat {
     String name;
     int appetite;
+    boolean satiety;
 
-    Cat(String name, int appetite) {
+    Cat(String name, int appetite, boolean satiety) {
         this.name = name;
         this.appetite = appetite;
+        this.satiety = satiety;
     }
-
-  void eat(Plate plate) {
-
-  }
-
-
-
-
-
+    public String toString() {
+        String info;
+        if (satiety) {
+             info = name + " может съесть за 1 раз " + appetite + " еды. сейчас он сыт";
+        } else {
+             info = name + " может съесть за 1 раз " + appetite + " еды. сейчас он голоден";
+        }
+        return info;
+    }
 }
--- src/main/java/ru/otus/java/basic/homeworks/homework6/Plate.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework6/Plate.java
@@ -11,25 +11,33 @@ class Plate {
         this.actualFood = actualFood;
         this.packageCounter = packageCounter;
         this.capacityPackage = capacityPackage;
-
     }
     @Override
     public String toString() {
-        return "В миске осталось " + actualFood + ". В запасе ещё " + packageCounter + " пакетов по " + capacityPackage + " еды";
+        return "В миске " + actualFood + " еды. В запасе ещё " + packageCounter + " пакетов по " + capacityPackage + " еды";
     }
-
-
-
-    public void putFood() {
-        if (actualFood + capacityPackage > maxValueFood) {
-            System.out.println("В миске достаточно еды.");
+    boolean decreaseFood(Cat cat) {
+        if (actualFood - cat.appetite >= 0) {
+            actualFood -= cat.appetite;
+            System.out.println(cat.name + " пока что наелся");
+            cat.satiety = true;
         } else {
-          actualFood += capacityPackage;
-          packageCounter--;
-            System.out.println("Вы насыпали еду в миску. Осталось " + packageCounter + " пакетов еды");
+            System.out.println(cat.name + " не выдержал такого неуважения, потому что для него в тарелке не хватает еды. Он выбирает голодную смерть %)");
+            cat.satiety = false;
         }
-
+        return cat.satiety;
     }
-
-
+    public void putFood() {
+        if (packageCounter > 0) {
+            if (actualFood + capacityPackage > maxValueFood) {
+                System.out.println("В миске достаточно еды.");
+            } else {
+                actualFood += capacityPackage;
+                packageCounter--;
+                System.out.println("Вы насыпали еду в миску. Осталось " + packageCounter + " пакетов еды.\nВ миске теперь " + actualFood + " еды");
+            }
+        } else {
+            System.out.println("Милорд, запасы провизии на исходе. Коты требуют заказать им ещё еды.\nВ миске " + actualFood + " еды");
+        }
+    }
 }
