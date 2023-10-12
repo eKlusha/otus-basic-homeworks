b55a5c37967b22df15889745b3b7d46c0f91b28f
eKlusha <noobrus86@yandex.ru>
2023-10-12 18:55:09 +0300

    hw9 end

--- src/main/java/ru/otus/java/basic/homeworks/homework9/HomeWork9.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework9/HomeWork9.java
@@ -1,7 +1,45 @@
 package ru.otus.java.basic.homeworks.homework9;
 
+import java.util.*;
+
 public class HomeWork9 {
+
     public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Worker worker1 = new Worker("Sam", 34);
+        Worker worker2 = new Worker("Jim", 40);
+        Worker worker3 = new Worker("Jack", 30);
+        Worker worker4 = new Worker("Peter", 20);
+        Worker worker5 = new Worker("Bill", 58);
+        Worker worker6 = new Worker("Joseph", 24);
+        Worker worker7 = new Worker("David", 28);
+
+        Methods.minMax(4, 3, numbers);
+        Methods.minMax(2, 7, numbers);
+        Methods.minMax(1, 0, numbers);
+        Methods.minMax(3, 4, numbers);
+        Methods.minMax(7, 7, numbers);
+        System.out.println(Methods.sumOfNumbers(numbers));
+        Methods.resetNumbers(0, numbers);
+        Methods.changeNumbers(4, numbers);
+
+        List<Worker> Workers = new ArrayList<>();
+        Workers.add(worker1);
+        Workers.add(worker2);
+        Workers.add(worker3);
+        Workers.add(worker4);
+        Workers.add(worker5);
+        Workers.add(worker6);
+        Workers.add(worker7);
+        System.out.println(Methods.names(Workers));
+        System.out.println(Methods.minAgeList(25,Workers));
+        Methods.average(30,Workers);
+        Methods.average(40,Workers);
+        System.out.println(Methods.young(Workers));
+
+
+
+
 
     }
 }
---
+++ src/main/java/ru/otus/java/basic/homeworks/homework9/Methods.java
@@ -0,0 +1,110 @@
+package ru.otus.java.basic.homeworks.homework9;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Methods {
+    public static void minMax(Integer a, Integer b, List<Integer> list) {
+
+        if (list.size() == 0) {
+            list.add(a);
+            if (list.get(0) >= b) {
+                list.add(0, b);
+                System.out.println(list);
+            }
+        } else {
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (a >= list.get(list.size() - 1)) {
+                    list.add(a);
+                    break;
+                } else if (a <= list.get(0)) {
+                    list.add(0, a);
+                    break;
+                } else if (list.get(i) >= a && a <= list.get(i + 1)) {
+                    list.add(i + 1, a);
+                    break;
+                }
+            }
+            System.out.println(list);
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (b >= list.get(list.size() - 1)) {
+                    list.add(b);
+                    break;
+                } else if (b <= list.get(0)) {
+                    list.add(0, b);
+                    break;
+                } else if (list.get(i) >= b && b <= list.get(i + 1)) {
+                    list.add((i + 1), b);
+                    break;
+                }
+            }
+            System.out.println(list);
+        }
+    }
+
+    public static int sumOfNumbers(List<Integer> list) {
+        int sum = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) >= 5) {
+                sum += list.get(i);
+            }
+        }
+        return sum;
+    }
+
+    public static void resetNumbers(int a, List<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            list.set(i, a);
+        }
+        System.out.println(list);
+    }
+
+    public static void changeNumbers(int a, List<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
+            list.set(i, list.get(i) + a);
+        }
+        System.out.println(list);
+    }
+
+    public static List names(List<Worker> Workers) throws NullPointerException {
+        List<String> names = new ArrayList<>();
+        for (int i = 0; i < Workers.size(); i++) {
+            names.add(Workers.get(i).getName());
+        }
+        return names;
+    }
+
+    public static List minAgeList(int minAge, List<Worker> Workers) {
+        List<String> names = new ArrayList<>();
+        for (int i = 0; i < Workers.size(); i++) {
+            if (Workers.get(i).getAge() >= minAge) {
+                names.add(Workers.get(i).getName());
+            }
+        }
+        return names;
+    }
+
+    public static void average(int age, List<Worker> workers) {
+        int middleAgeWorkers = 0;
+        for (int i = 0; i < workers.size(); i++) {
+            middleAgeWorkers += workers.get(i).getAge();
+        }
+        if (middleAgeWorkers / workers.size() > age) {
+            System.out.println("средний возраст сотрудников превышает " + age);
+        } else {
+            System.out.println("средний возраст сотрудников не превышает " + age);
+        }
+    }
+    public static Worker young (List<Worker> workers) {
+        int age = workers.get(0).getAge();
+        Worker youngWorker = workers.get(0);
+        for (int i = 1; i < workers.size(); i++) {
+            if (workers.get(i).getAge() <= age) {
+                age = workers.get(i).getAge();
+                youngWorker = workers.get(i);
+            }
+        }
+        return youngWorker;
+    }
+}
+
